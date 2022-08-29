ee6210a0cd94caca06f5f4f139c2efd88e9b0fa6
Noah Wagner <wagnernoah@icloud.com>
2022-08-29 14:13:09 -0700

    first commit

---
+++ Raycaster/examples/example.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../include/raycaster.h"
+
+int main() {
+	return 0;
+}
---
+++ VoxelSpace/examples/example.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../include/voxelspace.h"
+
+int main() {
+	return 0;
+}
