a9f46b49f6fcc7e9b63f31b956b67340329b2f57
Noah Wagner <wagnernoah@icloud.com>
2022-08-31 15:23:27 -0700

    added cast ray functions, hit info struct, and comments

--- Raycast/include/raycast.h
+++ Raycast/include/raycast.h
@@ -3,34 +3,71 @@
 
 #include <stdint.h>
 
+/*
+all the possible faces a ray can hit
+top and bottom being the floor and ceiling
+*/
 typedef enum {
-	raycast_north, 
-	raycast_south, 
-	raycast_east, 
-	raycast_west, 
-	raycast_top, 
+	raycast_north,
+	raycast_south,
+	raycast_east,
+	raycast_west,
+	raycast_top,
 	raycast_bottom
 } raycast_face_t;
 
+// simple 32bit color struct
 typedef struct {
 	uint8_t r, g, b, a;
 } raycast_color_t;
 
+/*
+this function runs for every pixel on the screen that coorelates to a surface, whether it be wall, floor, or ceiling
+it provides information about that pixels location in the world through its arguments
+the user can use this information to map textures to surfaces, implement depth lighting,
+source lighting, glossy floors, sunlight shadows, and more
+the first parameter is the pixel that the user is supposed to change
+*/
 typedef void (*surface_pixel_t)(raycast_color_t*, int map_x, int map_y, double unit_x, double unit_y, raycast_face_t face, double depth);
+
+/*
+similar to the function above, but it instead runs for every pixel on the screen
+that coorelates to a sprite in the raycast scene
+*/
 typedef void (*sprite_pixel_t)(raycast_color_t*, double unit_x, double unit_y, double depth);
 
+/*
+used by cast ray functions, holds relavent
+information about the hit location of the ray
+*/
+typedef struct {
+	uint32_t map_x, map_y;
+	double distance;
+	raycast_face_t face;
+} raycast_hit_info_t;
+
+// simple 2d point struct
 typedef struct {
 	double x, y;
 } raycast_point_t;
 
+// vector is the same as point, only used to distinguish the intent of a variable
 typedef raycast_point_t raycast_vector_t;
 
+/*
+objects represent basic information about sprites in the scene,
+its up to the user to provide the texture of the object
+*/
 typedef struct {
 	int id;
 	raycast_point_t position;
 	double size;
 } raycast_object_t;
 
+/*
+the renderer is responsible for storing information about the screen,
+certain render settings, and the two pixel functions provided by the user
+*/
 typedef struct {
 	uint32_t width, height;
 	double aspect_ratio;
@@ -41,6 +78,10 @@ typedef struct {
 	sprite_pixel_t sprite_pixel;
 } raycast_renderer_t;
 
+/*
+the scene is responsible for storing information about
+the geometry of the world, and the objects in the scene
+*/
 typedef struct {
 	char *world_map;
 	uint32_t world_width, world_height;
@@ -48,26 +89,66 @@ typedef struct {
 	double wall_height, top_height, bottom_height;
 } raycast_scene_t;
 
+/*
+the camera is responsible for storing information about where
+the world will be rendered, and what fov it will be rendered at
+*/
 typedef struct {
 	raycast_point_t position;
 	raycast_vector_t direction;
+	raycast_vector_t plane;
 	double pitch, height, focalLength;
 } raycast_camera_t;
 
+// init functions
 void raycast_object_init(raycast_object_t*, int id, double x, double y);
 void raycast_scene_init(raycast_scene_t*, char *world_map, raycast_object_t *objects);
 void raycast_camera_init(raycast_camera_t*, double x, double y, double aspect_ratio);
 void raycast_renderer_init(raycast_renderer_t*, uint32_t *pixel_data, uint32_t width, uint32_t height, surface_pixel_t surface_pixel, sprite_pixel_t sprite_pixel);
 
+// de-init functions
 void raycast_renderer_free(raycast_renderer_t*);
 
+
+// camera movement functions
+
+/*
+user cannot directly manipulate the direction
+attribute of the camera because camera plane must be perpendicular
+to camera direction at all times, hence why these functions exist
+*/
 void raycast_camera_set_dir(raycast_camera_t*, double x, double y);
 void raycast_camera_rotate(raycast_camera_t*, double angle);
 void raycast_camera_set_rotation(raycast_camera_t*, double angle);
 
+// cast ray functions
+
+/*
+only used internally by the render engine, faster but it requires specific setup
+by the camera, and it doesn't calculate the true euclidean distance traveled by the ray
+uses hit_info in first parameter to return information about where the ray hit
+*/
+void raycast_camera_cast_ray(raycast_hit_info_t*, raycast_camera_t*, raycast_scene_t*);
+
+/*
+cast ray function intended for the user to use, slower than the previous
+function but it gives more practical information, information returned regarding
+where the ray hit is stored in the hit_info struct
+*/
+void raycast_cast_ray(raycast_hit_info_t*, raycast_scene_t*, double pos_x, double pos_y, double dir_x, double dir_y);
+
+/*
+casts a ray from one point to another, returns 1 if there
+is an obstruction, and 0 if there is no obstruction
+*/
+int raycast_check_obstruction(double start_x, double start_y, double end_x, double end_y, raycast_scene_t*);
+
+// render functions
 void raycast_render_walls(raycast_renderer_t*, raycast_scene_t*, raycast_camera_t*);
 void raycast_render_top_bottom(raycast_renderer_t*, raycast_scene_t*, raycast_camera_t*);
 void raycast_render_sprites(raycast_renderer_t*, raycast_scene_t*, raycast_camera_t*);
+
+// runs all three previous render function to draw a complete world
 void raycast_render(raycast_renderer_t*, raycast_scene_t*, raycast_camera_t*);
 
 #endif
