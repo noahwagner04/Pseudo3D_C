c4827486a4588d4c98e8aa26ec2fa04060f4e6ea
Noah Wagner <wagnernoah@icloud.com>
2022-09-02 14:53:06 -0700

    implemented cast ray functions

--- Raycast/src/raycast.c
+++ Raycast/src/raycast.c
@@ -107,16 +107,115 @@ void raycast_camera_set_rotation(raycast_camera_t* camera, double angle) {
 
 // cast ray functions
 
-void raycast_camera_cast_ray(raycast_hit_info_t* hit_info, raycast_camera_t* camera, raycast_scene_t* scene) {
+void raycast_DDA(raycast_hit_info_t* hit_info, raycast_scene_t* scene, double pos_x, double pos_y, double dir_x, double dir_y, double initial_ray_length) {
+	int map_x = (int)pos_x;
+	int map_y = (int)pos_y;
 
+	double side_dist_x;
+	double side_dist_y;
+
+	int step_x;
+	int step_y;
+
+	double delta_dist_x = fabs(initial_ray_length / dir_x);
+	double delta_dist_y = fabs(initial_ray_length / dir_y);
+
+	uint8_t hit = 0;
+	uint8_t side;
+
+	// DDA setup step directions and initial side distances
+	if (dir_x < 0) {
+		step_x = -1;
+		side_dist_x = (pos_x - map_x) * delta_dist_x;
+	} else {
+		step_x = 1;
+		side_dist_x = (map_x + 1.0 - pos_x) * delta_dist_x;
+	}
+	if (dir_y < 0) {
+		step_y = -1;
+		side_dist_y = (pos_y - map_y) * delta_dist_y;
+	} else {
+		step_y = 1;
+		side_dist_y = (map_y + 1.0 - pos_y) * delta_dist_y;
+	}
+
+	// perform DDA
+	while (hit == 0) {
+		// increment ray position
+		if (side_dist_x > side_dist_y) {
+			map_y += step_y;
+			side_dist_y += delta_dist_y;
+			side = 1;
+		} else {
+			map_x += step_x;
+			side_dist_x += delta_dist_x;
+			side = 0;
+		}
+		// check if ray is out of map bounds, manually stop casting if so
+		if (map_x < 0 || map_y < 0 || map_x >= scene->world_width || map_y >= scene->world_height) {
+			break;
+		}
+		hit = scene->world_map[map_x + scene->world_width * map_y];
+	}
+
+	// calculate distance the ray traveled
+	if (side == 0) {
+		hit_info->distance = side_dist_x - delta_dist_x;
+	} else {
+		hit_info->distance = side_dist_y - delta_dist_y;
+	}
+
+	// set other info in hit_info struct
+	hit_info->hit_point.x = map_x;
+	hit_info->hit_point.y = map_y;
+
+	hit_info->wall_type = hit;
+
+	// calculate face hit
+	if (side == 0) {
+		if (dir_x > 0) {
+			hit_info->face = raycast_east;
+		} else {
+			hit_info->face = raycast_west;
+		}
+	} else {
+		if (dir_y > 0) {
+			hit_info->face = raycast_south;
+		} else {
+			hit_info->face = raycast_north;
+		}
+	}
 }
 
 void raycast_cast_ray(raycast_hit_info_t* hit_info, raycast_scene_t* scene, double pos_x, double pos_y, double dir_x, double dir_y) {
+	double initial_ray_length = sqrt(dir_x * dir_x + dir_y * dir_y);
 
+	// cast ray
+	raycast_DDA(hit_info, scene, pos_x, pos_y, dir_x, dir_y, initial_ray_length);
+
+	// calculate the floating point location of where the ray hit
+	hit_info->hit_point.x = pos_x + dir_x / initial_ray_length * hit_info->distance;
+	hit_info->hit_point.y = pos_y + dir_y / initial_ray_length * hit_info->distance;
 }
 
 int raycast_check_obstruction(raycast_scene_t* scene, double start_x, double start_y, double end_x, double end_y) {
+	// set up variables for DDA function
+	double dir_x = end_x - start_x;
+	double dir_y = end_y - start_y;
 
+	double between_length = sqrt(dir_x * dir_x + dir_y * dir_y);
+
+	raycast_hit_info_t hit_info;
+
+	//cast ray
+	raycast_DDA(&hit_info, scene, start_x, start_y, dir_x, dir_y, between_length);
+
+	// check if ray hit end point
+	if(hit_info.distance < between_length) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 // render functions
